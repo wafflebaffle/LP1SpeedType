df079cf29eb3261699bc63e734addc40547c8a7b
nunes <vera.ml.nunes@gmail.com>
2025-04-11 13:30:28 +0100

    solution and project

---
+++ SpeedType/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpeedType
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
